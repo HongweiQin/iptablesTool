f6bbe5b315e8162235d0536d5cc6a470205d077b
HongweiQin <460897705@qq.com>
2016-09-10 23:16:17 +0800

    functions

--- iptablesTool.sh
+++ iptablesTool.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+function fhelp()
+{
+echo "portMap - map a LAN server port to a WAN port"
+echo "export - export current commands to a file"
+echo "? - print out available commands"
+echo "quit - Exit program"
+}
+
+function fshow()
+{
+
+}
+
+function pmap()
+{
+
+}
+
 echo "Welcome to use iptables tool!"
 echo "type ? for help"
 goon=1
@@ -7,11 +25,11 @@ do
 	printf "==>"
 	read cmd
  	case $cmd in
-		?) echo "in=1"
+		?) fhelp ;
 		;;
-		show) echo "in=2"
+		show) fshow ;
 		;;
-		portMap) echo "in=ss"
+		portMap) fpmap ;
 		;;
 	esac
 	break
